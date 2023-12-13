fc29e86fc67426bb95ea8132e132f5c5b123c433
Kavach1365 <152857440+Kavach1365@users.noreply.github.com>
2023-12-12 20:02:55 -0800

    Updated_Thesis2.js
    after MUI

--- Thesis2.js
+++ Thesis2.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FormControl, InputLabel, MenuItem, Select, Button, Typography, Grid, Box, Paper } from '@mui/material';
 
 export const Thesis2 = () => {
   const mainOptions = ['Domain 1', 'Domain 2', 'Domain 3'];
@@ -43,62 +44,109 @@ export const Thesis2 = () => {
   };
 
   return (
-    <div>
-      {mainOptions.map((mainOption, mainIndex) => (
-        <div key={mainIndex}>
-          <label>{`Domain Preference ${mainIndex + 1}`}</label>
-          <select
-            value={mainSelections[mainIndex]}
-            onChange={(e) => handleMainSelect(mainIndex, e.target.value)}
-          >
-            <option value="">select domain</option>
-            {mainOptions.map((option) => (
-              <option key={option} value={option} disabled={mainSelections.includes(option) && mainSelections.indexOf(option) !== mainIndex}>
-                {option}
-              </option>
-            ))}
-          </select>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="105vh"
+    >
+      <Paper elevation={8} style={{ width: '50%', padding: '20px', overflowY: 'auto', maxHeight: '80vh' }}>
+        <Grid container spacing={2}>
+          {mainOptions.map((mainOption, mainIndex) => (
+            <Grid item xs={12} key={mainIndex}>
+              <FormControl fullWidth variant="outlined" style={{ marginBottom: '10px' }}>
+                <InputLabel style={{ fontSize: '14px' }}>{`Domain Preference ${mainIndex + 1}`}</InputLabel>
+                <Select
+                  value={mainSelections[mainIndex]}
+                  onChange={(e) => handleMainSelect(mainIndex, e.target.value)}
+                  label={`Domain Preference ${mainIndex + 1}`}
+                  MenuProps={{
+                    PaperProps: {
+                      style: {
+                        maxHeight: 150,
+                      },
+                    },
+                  }}
+                >
+                  <MenuItem value="" style={{ height: '30px', fontSize: '12px' }}>
+                    <em>Select domain</em>
+                  </MenuItem>
+                  {mainOptions.map((option) => (
+                    <MenuItem
+                      key={option}
+                      value={option}
+                      disabled={mainSelections.includes(option) && mainSelections.indexOf(option) !== mainIndex}
+                      style={{ height: '30px', fontSize: '12px' }}
+                    >
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
 
-          {mainCompleted[mainIndex] && (
-            <div>
-              {subOptions[mainSelections[mainIndex]].map((subOption, subIndex) => (
-                <div key={subIndex}>
-                  <label>{`Guide Preference ${mainIndex + 1}.${subIndex + 1}`}</label>
-                  <select
-                    value={subSelections[mainIndex][subIndex]}
-                    onChange={(e) => handleSubSelect(mainIndex, subIndex, e.target.value)}
-                  >
-                    <option value="">select guide</option>
-                    {subOptions[mainSelections[mainIndex]].map((option) => (
-                      <option key={option} value={option} disabled={subSelections[mainIndex].includes(option)}>
-                        {option}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-      ))}
-
-      <button onClick={handleStorePreferences}>Store Preferences</button>
-
-      <div>
-        <h2>Stored Preferences</h2>
-        <ul>
-          {storedPreferences.map((preference, index) => (
-            <li key={index}>
-              <strong>{`Domain ${index + 1}: `}</strong> {preference.domain}
-              <ul>
-                {preference.guides.map((guide, subIndex) => (
-                  <li key={subIndex}>{`Guide ${index + 1}.${subIndex + 1}: `}{guide}</li>
-                ))}
-              </ul>
-            </li>
+              {mainCompleted[mainIndex] && (
+                <Grid item xs={12} style={{ width: '70%', marginLeft: '20%' }}>
+                  {subOptions[mainSelections[mainIndex]].map((subOption, subIndex) => (
+                    <FormControl fullWidth variant="outlined" key={subIndex} style={{ marginBottom: '10px' }}>
+                      <InputLabel style={{ fontSize: '14px' }}>{`Guide Preference ${mainIndex + 1}.${subIndex + 1}`}</InputLabel>
+                      <Select
+                        value={subSelections[mainIndex][subIndex]}
+                        onChange={(e) => handleSubSelect(mainIndex, subIndex, e.target.value)}
+                        label={`Guide Preference ${mainIndex + 1}.${subIndex + 1}`}
+                        MenuProps={{
+                          PaperProps: {
+                            style: {
+                              maxHeight: 150,
+                            },
+                          },
+                        }}
+                      >
+                        <MenuItem value="" style={{ height: '30px', fontSize: '12px' }}>
+                          <em>Select guide</em>
+                        </MenuItem>
+                        {subOptions[mainSelections[mainIndex]].map((option) => (
+                          <MenuItem
+                            key={option}
+                            value={option}
+                            disabled={subSelections[mainIndex].includes(option)}
+                            style={{ height: '30px', fontSize: '12px' }}
+                          >
+                            {option}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  ))}
+                </Grid>
+              )}
+            </Grid>
           ))}
-        </ul>
-      </div>
-    </div>
+
+          <Grid item xs={12} style={{ textAlign: 'center' }}>
+            <Button variant="contained" color="primary" onClick={handleStorePreferences}>
+              Store Preferences
+            </Button>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography variant="h6" style={{ textAlign: 'center' }}>
+              Stored Preferences
+            </Typography>
+            <ul>
+              {storedPreferences.map((preference, index) => (
+                <li key={index}>
+                  <strong>{`Domain ${index + 1}: `}</strong> {preference.domain}
+                  <ul>
+                    {preference.guides.map((guide, subIndex) => (
+                      <li key={subIndex}>{`Guide ${index + 1}.${subIndex + 1}: `}{guide}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Box>
   );
 };
