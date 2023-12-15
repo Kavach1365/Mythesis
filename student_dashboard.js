3f9970967f0307faad8a9c27accdf3f5230c8483
Kavach1365 <152857440+Kavach1365@users.noreply.github.com>
2023-12-15 17:59:29 +0530

    Add files via upload

--- student_dashboard.js
+++ student_dashboard.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import Calendar from 'react-calendar';
-import './StudentDashboard.css'; // Include your CSS styles for the dashboard if needed
-import 'react-calendar/dist/Calendar.css';
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import './StudentDashboard.css';
+//import "./App.css";
 
 // Dummy data for the student's current thesis and progress
 const currentThesis = {
@@ -13,21 +20,79 @@ const progressData = [
   { step: 1, label: 'Proposal', completed: true },
   { step: 2, label: 'Literature Review', completed: false },
   { step: 3, label: 'Data Collection', completed: false },
+  { step: 4, label: 'Proposal', completed: true },
+  { step: 5, label: 'Literature Review', completed: false },
+  { step: 6, label: 'Data Collection', completed: false },
   // Add more steps as needed
 ];
 
-const importantDates = [
-  new Date(2023, 11, 15), // Month is 0-indexed
-  new Date(2023, 11, 20),
-  // Add more important dates as needed
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2021, 6, 0),
+        end: new Date(2021, 6, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2021, 6, 7),
+        end: new Date(2021, 6, 10),
+    },
+    {
+        title: "Conference",
+        start: new Date(2021, 6, 20),
+        end: new Date(2021, 6, 23),
+    },
 ];
 
-// React functional component for the Student Dashboard
-export const StudentDashboard = () => {
-  return (
-    <div className="container">
-      {/* My Current Thesis and Calendar */}
-      <div className="card-container">
+export const StudentDashboard=()=> {
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        
+        for (let i=0; i<allEvents.length; i++){
+
+            const d1 = new Date (allEvents[i].start);
+            const d2 = new Date(newEvent.start);
+            const d3 = new Date(allEvents[i].end);
+            const d4 = new Date(newEvent.end);
+      /*
+          console.log(d1 <= d2);
+          console.log(d2 <= d3);
+          console.log(d1 <= d4);
+          console.log(d4 <= d3);
+            */
+
+             if (
+              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
+                (d4 <= d3) )
+              )
+            {   
+                alert("CLASH"); 
+                break;
+             }
+    
+        }
+        
+        
+        setAllEvents([...allEvents, newEvent]);
+    }
+
+    return (
+      <div>
+          <div className="card-container">
         {/* My Current Thesis */}
         <div className="card" style={{ minWidth: '400px' }}>
           <div className="card-header">
@@ -39,24 +104,26 @@ export const StudentDashboard = () => {
           </div>
         </div>
 
-        {/* Calendar */}
-        <div className="calendar-card">
-          <div className="calendar-header">
-            <h2 className="h4 mb-4">Calendar</h2>
-          </div>
-          <div className="calendar-body">
-            <Calendar
-              className="custom-calendar"
-              tileContent={({ date, view }) =>
-                view === 'month' && importantDates.some((d) => d.toDateString() === date.toDateString()) ? <div className="important-date-marker"></div> : null
-              }
-            />
-          </div>
+        <div className="calender-card" style={{ height: 500,width:300, margin: "50px" }}>
+            <h2>Calendar</h2>
+            <h3>Add New Event</h3>
+            <div className="calender-body">
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 400,marginTop:50}} />
         </div>
-      </div>
 
-      {/* Thesis Progress Tracking Stepper */}
-      <div className="card" style={{ maxWidth: '400px' }}>
+      </div>
+      
+
+      
+
+      <div className="card" style={{   }}>
         <div className="card-header">
           <h2 className="h4 mb-4">Progress Tracking</h2>
         </div>
@@ -73,5 +140,7 @@ export const StudentDashboard = () => {
         </div>
       </div>
     </div>
-  );
-};
+    );
+}
+
+//export default App;
